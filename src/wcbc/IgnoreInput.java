99893cc2790e677f13888140e073ba56d6cbead8
John MacCormick <jmac@dickinson.edu>
2018-05-18 12:51:42 +0100

    added comment to IgnoreInput.java

--- src/wcbc/IgnoreInput.java
+++ src/wcbc/IgnoreInput.java
@@ -2,10 +2,21 @@ package wcbc;
 
 import java.io.IOException;
 
+/**
+ * SISO program IgnoreInput.java
+ * 
+ * This program ignores its input parameter, instead executing a fixed
+ * pre-prepared computation based on a Java program and input string that have
+ * been stored on disk. The Java program P is stored in the file
+ * progString.txt and the input string I is stored in the file inString.txt.
+ * 
+ * inString: The input parameter which is ignored.
+ * 
+ * returns: P(I), where P and I are as described above.
+ *
+ */
 public class IgnoreInput implements Siso {
 
-	
-	
 	@Override
 	public String siso(String inString) throws WcbcException, IOException {
 		String progString = utils.readFile(utils.prependWcbcPath("progString.txt"));
@@ -14,13 +25,13 @@ public class IgnoreInput implements Siso {
 		String result = universal.siso(progString, newInString);
 		return result;
 	}
-	
+
 	public static void main(String[] args) throws IOException, WcbcException {
 		String progString = utils.readFile(utils.prependWcbcPath("containsGAGA.java"));
 		utils.writeFile(utils.prependWcbcPath("progString.txt"), progString);
 		String inString = "CCCCCCCCCCAAGAGATT";
 		utils.writeFile(utils.prependWcbcPath("inString.txt"), inString);
-		
+
 		IgnoreInput ignoreInput = new IgnoreInput();
 		String result = ignoreInput.siso("asdf");
 		System.out.println(result);
