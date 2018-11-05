c969b746f30d0ae07c870b6cc88bdacdce1dc9df
John MacCormick <jmac@dickinson.edu>
2018-11-05 12:59:52 +0000

    Implemented up to and including Path.rotateToFront
    Working my way through graph.py. No tests have been implemented
    yet. The next thing to do is finish Path and Edge (there is one more
    method in Path itself, then we need the ability to iterate over edges,
    then the very simple implementation of Edge). After that, implement
    all tests for Path and Edge. Then we will be able to move on to Graph,
    I think.

--- src/wcbc/Path.java
+++ src/wcbc/Path.java
@@ -1,13 +1,201 @@
 package wcbc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
 /**
  * A Path object models a path in a graph.
  *
  */
 public class Path {
 
-	public Path() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * List of the nodes in the path, where each node is a string.
+	 */
+	protected ArrayList<String> nodes = null;
+
+	/**
+	 * A set of strings that is constructed lazily (if and when needed). It will
+	 * contain the nodes in the path.
+	 */
+	protected HashSet<String> nodeSet = null;
+
+	/**
+	 * Set of Edge objects in the path. Constructed lazily (if and when needed).
+	 */
+	protected HashSet<Edge> edgeSet = null;
+
+	/**
+	 * Construct a Path from a list of nodes
+	 * 
+	 * @param nodes
+	 *            list of nodes in the path, where each node is a string
+	 * @throws WcbcException
+	 */
+	public Path(Collection<String> nodes) throws WcbcException {
+		for (String node : nodes) {
+			if (node.length() == 0) {
+				throw new WcbcException("Encountered empty node name");
+			}
+		}
+		this.nodes = new ArrayList<>(nodes);
+
+	}
+
+	@Override
+	public String toString() {
+		return utils.join(nodes, ",");
+	}
+
+	/**
+	 * @return the number of nodes in the path
+	 */
+	public int getLength() {
+		return nodes.size();
+	}
+
+	/**
+	 * @param index
+	 *            the index of the node to be returned
+	 * @return the index-th node in the path
+	 */
+	public String getNode(int index) {
+		return nodes.get(index);
+	}
+
+	/**
+	 * Return True if node is in this path, and False otherwise.
+	 * 
+	 * @param node
+	 *            The node whose presence is to be detected
+	 * @return True if node is in this path, and False otherwise
+	 */
+	public boolean containsNode(String node) {
+		if (this.nodeSet == null) {
+			nodeSet = new HashSet<>(nodes);
+		}
+		return nodeSet.contains(node);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nodes == null) ? 0 : nodes.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Path other = (Path) obj;
+		if (nodes == null) {
+			if (other.nodes != null)
+				return false;
+		} else if (!nodes.equals(other.nodes))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Construct a new Path object from a string and return it.
+	 * 
+	 * @param pathStr
+	 * @return This string should list the nodes in the path separated by commas.
+	 *         For example: "apple,banana,x,y,z" yields a path consisting of five
+	 *         nodes and four edges.
+	 * @throws WcbcException
+	 */
+	public static Path fromString(String pathStr) throws WcbcException {
+		ArrayList<String> nodes = new ArrayList<>();
+		if (pathStr.length() > 0) {
+			nodes.addAll(Arrays.asList(pathStr.split(",")));
+		}
+		return new Path(nodes);
+	}
+
+	/**
+	 * Return the initial node in the path.
+	 * 
+	 * @return the initial node in the path
+	 * @throws WcbcException
+	 *             if there are no nodes in the path
+	 */
+	public String start() throws WcbcException {
+		if (getLength() == 0) {
+			throw new WcbcException("can\"t find start of empty path");
+		}
+		return this.getNode(0);
+	}
+
+	/**
+	 * Return the final node in the path.
+	 * 
+	 * @return the final node in the path
+	 * @throws WcbcException
+	 *             if there are no nodes in the path
+	 */
+	public String end() throws WcbcException {
+		if (getLength() == 0) {
+			throw new WcbcException("can\"t find start of empty path");
+		}
+		return this.getNode(getLength() - 1);
+	}
+
+	/**
+	 * Extend the current path by appending the given node.
+	 * 
+	 * @param node
+	 *            The node to be added
+	 * @return A new Path object with the given node appended
+	 * @throws WcbcException
+	 */
+	public Path extend(String node) throws WcbcException {
+		ArrayList<String> newNodes = new ArrayList<>(this.nodes);
+		newNodes.add(node);
+		return new Path(newNodes);
+	}
+
+	/**
+	 * Reverse of the current path.
+	 * 
+	 * @return A new Path object which is the reverse of the current path.
+	 * @throws WcbcException
+	 */
+	public Path reversed() throws WcbcException {
+		ArrayList<String> reversedNodes = new ArrayList<>(this.nodes);
+		Collections.reverse(reversedNodes);
+		return new Path(reversedNodes);
+	}
+
+	/**
+	 * Cyclically permute the nodes in the path so that a given node is at the start
+	 * of the path.
+	 * 
+	 * @param node
+	 *            The node which should be at the start of the path.
+	 * @return A new Path object, cyclically permuted so the given node is at the
+	 *         start of the path.
+	 * @throws WcbcException
+	 *             is thrown if the given node is not in the path
+	 */
+	public Path rotateToFront(String node) throws WcbcException {
+		if (!this.containsNode(node)) {
+			String message = "node " + node + " is not in the path " + this.toString();
+			throw new WcbcException(message);
+		}
+		int ind = this.nodes.indexOf(node);
+		ArrayList<String> newNodes = new ArrayList<>(this.nodes.subList(ind, getLength()));
+		newNodes.addAll(this.nodes.subList(0, ind));
+		return new Path(newNodes);
 	}
 
 }
