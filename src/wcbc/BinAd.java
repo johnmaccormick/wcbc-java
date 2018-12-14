e1302be6c031f1fd62611b636b6ee1fe2700017d
John MacCormick <jmac@dickinson.edu>
2018-12-14 14:43:34 +0000

    Just started implementing BinAd

---
+++ src/wcbc/BinAd.java
@@ -0,0 +1,22 @@
+package wcbc;
+
+public class BinAd {
+
+	/**
+	 * The only axiom for the binAd system.
+	 */
+	public static final String theAxiom = "1=1";
+
+	private static final String binaryNumRegexString = "[01]+";
+
+	
+	public BinAd() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
